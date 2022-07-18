926d923368e436485de1736c33449cec3ad96fe6
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2022-07-18 02:59:25 +0100

    cc

---
+++ pages/blog/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <ContentList path="/blog" v-slot="{ list }">
+      <div v-for="article in list" :key="article._path">
+        <NuxtLink :to="article._path" class="text-black">
+          {{ article.title }}</NuxtLink
+        >
+        <h2>{{ article._path }}</h2>
+        <p>{{ article.description }}</p>
+      </div>
+    </ContentList>
+  </main>
+</template>
